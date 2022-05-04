35db94a1d6cac012bdc435895c978813ed0e7981
ABaldwin1297 <abaldwin1297@gmail.com>
2022-05-04 16:29:36 -0400

    Add project files.

---
+++ Banking/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking {
+    internal class Account {
+        private static int NextId = 1;
+        public int AcctNbr { get; private set; } = 0;
+        public string Name { get; set; } = string.Empty;
+        public decimal Balance { get; private set; } = 0;
+
+        public bool Deposit(decimal Amount) {
+            if(Amount <= 0) {
+                Console.WriteLine("Amount must be > zero");
+                return false;
+            }
+            Balance = Balance + Amount;
+            return true;
+        }
+
+        public bool Withdraw(decimal Amount) {
+            if (Amount <= 0) {
+                Console.WriteLine("Amount must be > zero");
+                return false;
+            }
+                if (Amount > Balance) {
+                Console.WriteLine("Insufficient Funds");
+                return false;
+            }
+            Balance = Balance - Amount;
+            return true ;
+        }
+
+        public bool Transfer(decimal Amount, Account ToAccount) {
+            var Sucess = Withdraw(Amount);
+            if (Sucess) {
+                ToAccount.Deposit(Amount);
+                return true;
+            }
+            return false;
+        }
+
+        public Account() {
+            AcctNbr = NextId++;
+        }
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,25 @@
+﻿using Banking;
+
+var acct1 = new Account() {
+    Name = "Checking 1"
+};
+var acct2 = new Account() {
+    Name = "Checking 2"
+};
+
+acct1.Deposit(1000);
+acct2.Deposit(500);
+
+acct1.Withdraw(200);
+acct2.Withdraw(300);
+
+Console.WriteLine($"Acct1 balance is {acct1.Balance:c}, Acct2 balance is {acct2.Balance:c}");
+
+acct1.Withdraw(1200); // shows insufficient funds b/c account only has $1,000
+Console.WriteLine($"Acct1 balance is {acct1.Balance:c}, Acct2 balance is {acct2.Balance:c}");
+
+acct2.Transfer(100, acct1);
+Console.WriteLine($"Acct1 balance is {acct1.Balance:c}, Acct2 balance is {acct2.Balance:c}");
+
+acct1.Deposit(100);
+Console.WriteLine($"Acct1 balance is {acct1.Balance:c}, Acct2 balance is {acct2.Balance:c}");
